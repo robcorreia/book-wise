cb15d2b774b92c4dbdf8dc862c1c07108ccf18a5
Robson <robccorreia@gmail.com>
2023-11-20 18:30:28 -0300

    feat: setup project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import { globalStyles } from "@/styles/global";
+import type { AppProps } from "next/app";
+
+import { Nunito } from "next/font/google";
+
+export const nunito = Nunito({ subsets: ["latin"] });
+
+globalStyles();
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <div className={nunito.className}>
+      <Component {...pageProps} />
+    </div>
+  );
+}
