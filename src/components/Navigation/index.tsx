02875b81e2d03d79f54e6eb249e6414b91724ac6
Robson <robccorreia@gmail.com>
2023-11-29 21:42:59 -0300

    feat: add Navigation component

---
+++ src/components/Navigation/index.tsx
@@ -0,0 +1,50 @@
+import { useMemo } from "react";
+import { ChartLineUp, Binoculars, User } from "@phosphor-icons/react";
+import { Container, NavItemContainer } from "./style";
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
+
+const NAV_ITEMS = [
+  {
+    label: "Início",
+    href: "/",
+    icon: <ChartLineUp size={24} />,
+  },
+  {
+    label: "Explorar",
+    href: "/explore",
+    icon: <Binoculars size={24} />,
+  },
+];
+
+export const Navigation = () => {
+  const router = useRouter();
+
+  const { data: session } = useSession();
+
+  const navItems = useMemo(() => {
+    if (session) {
+      return NAV_ITEMS.concat({
+        label: "Perfil",
+        href: `/profile/${session.user.id}`,
+        icon: <User size={24} />,
+      });
+    }
+    return NAV_ITEMS;
+  }, [session]);
+
+  return (
+    <Container>
+      {navItems.map(({ href, label, icon }) => (
+        <NavItemContainer
+          href={href}
+          key={label}
+          active={router.asPath === href}
+        >
+          {icon}
+          {label}
+        </NavItemContainer>
+      ))}
+    </Container>
+  );
+};
